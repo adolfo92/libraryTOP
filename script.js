d76291b06e0524989937d7100fbaaf7cbe9eb9f9
Adolfo92 <adolfo92dev@gmail.com>
2023-02-01 12:01:15 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const myLibrary = [];
+
+const bookShelf = document.querySelector(".bookShelf");
+
+function MakeBookCardDiv() {
+  const newCard = document.createElement("div");
+  newCard.classList.add("bookCard");
+  return newCard;
+}
+
+function printBookData(htmlTag, ObjectKey, object, container) {
+  const title = document.createElement(htmlTag);
+  title.textContent = `${ObjectKey}: ${object[ObjectKey]}`;
+  title.classList.add(`${ObjectKey}`);
+  container.appendChild(title);
+}
+
+class Book {
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+Book.prototype.MakeDeleteButton = function () {
+  const button = document.createElement("button");
+  button.textContent = "Eliminar libro";
+  button.classList.add("deleteButton");
+  button.addEventListener("click", () => {
+    myLibrary.splice(
+      myLibrary.findIndex((element) => element === this),
+      1
+    );
+    bookShelf.removeChild(document.querySelector(`.${this.title}`));
+  });
+
+  return button;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBook() {
+  if (myLibrary.length > 0) {
+    myLibrary.forEach((item) => {
+      const bookCard = bookShelf.appendChild(MakeBookCardDiv());
+      bookCard.classList.add(`${item.title}`);
+
+      const keys = Object.keys(item);
+
+      keys.forEach((key) => printBookData("h2", key, item, bookCard));
+
+      bookCard.appendChild(item.MakeDeleteButton());
+    });
+  }
+}
+//---------------------
